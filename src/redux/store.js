43c3f45d5e9867079eac0bbfb90fd5cbb3af5bee
srrobin007 <srrobin0007@gmail.com>
2023-10-19 20:36:28 +0600

    react devtool npm added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer ";
-import logger from 'redux-logger'
+import logger from 'redux-logger';
+import { composeWithDevTools } from '@redux-devtools/extension';
 // import myLogger from "./customMiddleWare/myLogger";
 
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
